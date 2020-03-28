cebd4f037dafa1ab846da6668569e6d1ad3b6e57
ritz-786 <ritikraj7255@gmail.com>
2020-03-28 10:03:17 +0530

    Added Navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { BrowserRouter,Route,Redirect,Switch } from 'react-router-dom'
+import './App.css'
+import AuthPage from './pages/Auth'
+import EventsPage from './pages/Events'
+import { BookingsPage } from './pages/Bookings'
+import MainNavigation from './components/Navigation/MainNavigation'
+
+export class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path='/auth' component={AuthPage} />
+              <Route path='/events' component={EventsPage} />
+              <Route path='/bookings' component={BookingsPage} />
+            </Switch>
+          </main>
+        </BrowserRouter>
+    )
+  }
+}
+
+export default App
---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class AuthPage extends Component {
+
+    render() {
+        return (
+            <div>
+                <h3>The Auth Page</h3>
+            </div>
+        )
+    }
+}
+
+export default AuthPage;
